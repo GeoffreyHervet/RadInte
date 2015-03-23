05fbb5b458368cd8d8614edb6541c6f896f3b430
Geoffrey Hervet <hervetg@gmail.com>
2015-03-23 17:25:47 +0100

    End

---
+++ blocks/product.php
@@ -0,0 +1,33 @@
+<?php
+    $kinds = ['instock', 'promo', 'new', 'gone', 'limited'];
+    $kind = $kinds[mt_rand(0, sizeof($kinds) - 1)];
+?>
+<div itemscope itemtype="http://schema.org/Product">
+    <meta itemprop="brand" content="Petit-bâteau" />
+    <meta itemprop="priceCurrency" content="EUR" />
+    <meta itemprop="availability" content="http://schema.org/<?php echo ($kind == 'gone') ? 'OutOfStock' : 'InStock'; ?>" />
+    <meta itemprop="image" content="img/tmp/product-list-250x325.jpg" />
+
+    <a href="?p=product" class="product">
+        <img  itemprop="image" src="img/tmp/product-list-250x325.jpg" alt="Executive Anvil logo" class="grayscale<?php if($kind == 'gone') echo '-100'; ?> no-grayscale-hover"/>
+        <span itemprop="name" class="name">Vers l’infini et au delà t-shirt</span>
+        <span class="price" itemprop="price" content="29.90">
+            <?php if ($kind == 'promo') { ?>
+                <strong class="text-pink">29,90 €</strong>
+                <span class="old-price">34,99 €</span>
+            <?php } else { ?>
+                29,90 €
+            <?php } ?>
+        </span>
+        <?php if ($kind == 'limited') { ?><span class="flash limited">édition limitée</span><?php } ?>
+        <?php if ($kind == 'new') { ?><span class="flash new">nouveauté</span><?php } ?>
+        <?php if ($kind == 'gone') { ?><span class="flash new">produit épuisé</span><?php } ?>
+    </a>
+    <?php if ($kind != 'gone') { ?>
+        <div class="btn-actions text-uppercase">
+            <a class="fast-buy" href="./">Achat rapide</a>
+            <a class="save" href="./">Sauvegarder</a>
+            <div class="clearfix"></div>
+        </div>
+    <?php } ?>
+</div>
---
+++ pages/category.php
@@ -0,0 +1,139 @@
+<div class="container">
+    <div class="text-top">
+        <div class="text-center">
+            <span>
+                Invitez vos amis et gagnez 50€ de crédit d’achat -
+                <a href="#"><strong>Acheter une carte cadeau</strong></a>
+            </span>
+        </div>
+        <span class="pull-right text-right">
+            <span class="date">Mer. 25 Octobre 10:32 - expédié en 48hrs</span><br />
+            <a href="#">Service client</a>
+        </span>
+    </div>
+</div>
+<div class="container">
+    <nav class="categories">
+        <ul>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">Nouveautés</a>
+                <ul class="dropdown-menu dropdown-menu-left" role="menu">
+                    <li><a href="#">Voir tout</a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">Les tendances</a>
+                <ul class="dropdown-menu dropdown-menu-left" role="menu">
+                    <li><a href="#">Voir tout</a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">Colabs</a>
+                <ul class="dropdown-menu dropdown-menu-left" role="menu">
+                    <li><a href="#">Voir tout</a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">Femme</a>
+                <ul class="dropdown-menu dropdown-menu-left" role="menu">
+                    <li><a href="#">Voir tout</a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">Homme</a>
+                <ul class="dropdown-menu dropdown-menu-left" role="menu">
+                    <li><a href="#">Voir tout</a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">Kids</a>
+                <ul class="dropdown-menu dropdown-menu-left" role="menu">
+                    <li><a href="#">Voir tout</a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">Sport</a>
+                <ul class="dropdown-menu dropdown-menu-left" role="menu">
+                    <li><a href="#">Voir tout</a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">Histoires</a>
+                <ul class="dropdown-menu dropdown-menu-left" role="menu">
+                    <li><a href="#">Voir tout</a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle text-pink" data-toggle="dropdown" role="button">Promotions</a>
+                <ul class="dropdown-menu dropdown-menu-right" role="menu">
+                    <li><a href="#">Voir tout</a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                    <li><a href="#">Women <span class="text-gray">(13)</span></a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</div>
+<div class="container category">
+    <div class="page-title text-underline text-center">Robe femme</div>
+    <div class="row" itemtype="http://schema.org/ItemList">
+        <meta itemprop="numberOfItems" content="315" />
+        <?php for ($i = 0; $i < 11; $i++) { ?>
+            <div class="col-md-3 col-sm-4 col-xs-6">
+                <div class="item">
+                    <?php include 'blocks/product.php'; ?>
+                </div>
+            </div>
+        <?php } ?>
+        <div class="col-md-3 col-sm-4 col-xs-6">
+            <div class="item">
+                <?php include 'blocks/product-gift.php'; ?>
+            </div>
+        </div>
+    </div>
+</div>
+<nav class="text-center">
+    <ul class="pagination hide">
+        <li>
+            <a href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+            </a>
+        </li>
+        <li class="active"><a href="#">1</a></li>
+        <li><a href="#">2</a></li>
+        <li><a href="#">3</a></li>
+        <li><a href="#">4</a></li>
+        <li><a href="#">5</a></li>
+        <li>
+            <a href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+            </a>
+        </li>
+    </ul>
+    <div class="infinite-animation clearfix"><div class="anim"></div></div>
+</nav>
