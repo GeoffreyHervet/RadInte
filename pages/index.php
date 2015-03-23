dca9c87777c868ebafabd279c4f5effda982f5e9
Geoffrey Hervet <hervetg@gmail.com>
2015-03-23 17:23:07 +0100

    Add alt to imgs

--- pages/index.php
+++ pages/index.php
@@ -102,7 +102,7 @@
 
 <div class="container">
     <a class="big-image img-text" href="#">
-        <img src="img/tmp/brand-xxl-1020-572.jpg" />
+        <img src="img/tmp/brand-xxl-1020-572.jpg" alt="Nouvelle collection" />
         <span class="texts">
             <strong>Nouvelle collection</strong>
             <span class="link">Acheter maintenant</span>
@@ -111,35 +111,35 @@
 </div>
 <div class="container imgs-text">
     <a class="img-text img-vertical no-left" href="#">
-        <img src="img/tmp/brand-l-335-500.jpg" />
+        <img src="img/tmp/brand-l-335-500.jpg" alt="Nouvelle collection" />
         <span class="texts white">
             <strong>Nouvelle collection</strong>
             <span class="link">Acheter maintenant</span>
         </span>
     </a>
     <a class="img-text img-vertical" href="#">
-        <img src="img/tmp/brand-l-335-500.jpg" />
+        <img src="img/tmp/brand-l-335-500.jpg" alt="Nouvelle collection" />
         <span class="texts">
             <strong>Nouvelle collection</strong>
             <span class="link">Acheter maintenant</span>
         </span>
     </a>
     <a class="img-text img-vertical no-right" href="#">
-        <img src="img/tmp/brand-l-335-500.jpg" />
+        <img src="img/tmp/brand-l-335-500.jpg" alt="Nouvelle collection" />
         <span class="texts white">
             <strong>Nouvelle collection</strong>
             <span class="link">Acheter maintenant</span>
         </span>
     </a>
     <a class="img-text img-horizontal no-left" href="#">
-        <img src="img/tmp/brand-w-505-310.jpg" />
+        <img src="img/tmp/brand-w-505-310.jpg" alt="Nouvelle collection" />
         <span class="texts">
             <strong>Nouvelle collection</strong>
             <span class="link">Acheter maintenant</span>
         </span>
     </a>
     <a class="img-text img-horizontal no-right" href="#">
-        <img src="img/tmp/brand-w-505-310.jpg" />
+        <img src="img/tmp/brand-w-505-310.jpg" alt="Nouvelle collection" />
         <span class="texts white">
             <strong>Nouvelle collection</strong>
             <span class="link">Acheter maintenant</span>
