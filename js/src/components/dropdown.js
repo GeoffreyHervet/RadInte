82f5e66bca40a9bf2323593e83b6a902084a8294
Geoffrey Hervet <hervetg@gmail.com>
2015-04-08 17:30:08 +0200

    Menu responsive

--- js/src/components/dropdown.js
+++ js/src/components/dropdown.js
@@ -1,10 +1,21 @@
+;'use strict';
 (function(){
-	$('li.dropdown').hover(function(){
-        var $this = $(this);
-        setTimeout(function(){
-            $this.addClass('open');
-        }, 100);
-	}, function(){
-		$(this).removeClass('open');
-	});
+    var $window = $(window);
+
+    var isEnabled = function(){
+        return $window.width() > 767;
+    };
+
+    $('li.dropdown').hover(function(){
+        if (isEnabled()) {
+            var $this = $(this);
+            setTimeout(function () {
+                $this.addClass('open');
+            }, 100);
+        }
+    }, function(){
+        if (isEnabled()) {
+            $(this).removeClass('open');
+        }
+    });
 })();
--- js/src/components/mobile-menus.js
+++ js/src/components/mobile-menus.js
@@ -1,44 +1,54 @@
 (function(){
-	$('#left-menu-toggler').click(function(e){
-		e.preventDefault();
-		var leftBar     = $('nav.categories');
-		var toMove      = $('#site-wrapper,.navbar-fixed-top');
-		var isShowhing  = !(leftBar.data('displayed') == 1);
-		console.log(isShowhing);
+    var $html = $('html');
+    $('#left-menu-toggler').click(function(e){
+        $html.toggleClass('nav-left');
+    });
 
-		leftBar.css('left', !isShowhing ? 0 : -300);
-		toMove.css({
-			'margin-left': !isShowhing ? 300 : 0,
-			'margin-right': !isShowhing ? -300 : 0
-		});
-		leftBar.animate({
-			left: !isShowhing ? -300 : 0
-		}, {duration: 200, queue: false});
-		toMove.animate({
-			marginLeft: isShowhing ? 300 : 0,
-			marginRight: isShowhing ? -300 : 0
-		}, {duration: 200, queue: false});
-		leftBar.data('displayed', isShowhing);
-	});
 
-	$('#right-menu-toggler').click(function(e){
-		e.preventDefault();
-		var rightBar     = $('.navbar .navbar-right');
-		var toMove      = $('#site-wrapper,.navbar-fixed-top');
-		var isShowhing  = !(rightBar.data('displayed') == 1);
+    $('#right-menu-toggler').click(function(e){
+        $html.toggleClass('nav-right');
+    });
 
-		rightBar.css('right', !isShowhing ? 0 : -300);
-		toMove.css({
-			'margin-right': !isShowhing ? 300 : 0,
-			'margin-left': !isShowhing ? -300 : 0
-		});
-		rightBar.animate({
-			right: !isShowhing ? -300 : 0
-		}, {duration: 200, queue: false});
-		toMove.animate({
-			marginRight: isShowhing ? 300 : 0,
-			marginLeft: isShowhing ? -300 : 0
-		}, {duration: 200, queue: false});
-		rightBar.data('displayed', isShowhing);
-	});
+    //$('#left-menu-toggler').click(function(e){
+    //    e.preventDefault();
+    //    var leftBar     = $('nav.categories');
+    //    var toMove      = $('#site-wrapper,.navbar-fixed-top');
+    //    var isShowhing  = !(leftBar.data('displayed') == 1);
+    //    console.log(isShowhing);
+    //
+    //    leftBar.css('left', !isShowhing ? 0 : -300);
+    //    toMove.css({
+    //        'margin-left': !isShowhing ? 300 : 0,
+    //        'margin-right': !isShowhing ? -300 : 0
+    //    });
+    //    leftBar.animate({
+    //        left: !isShowhing ? -300 : 0
+    //    }, {duration: 200, queue: false});
+    //    toMove.animate({
+    //        marginLeft: isShowhing ? 300 : 0,
+    //        marginRight: isShowhing ? -300 : 0
+    //    }, {duration: 200, queue: false});
+    //    leftBar.data('displayed', isShowhing);
+    //});
+    //
+    //$('#right-menu-toggler').click(function(e){
+    //    e.preventDefault();
+    //    var rightBar     = $('.navbar .navbar-right');
+    //    var toMove      = $('#site-wrapper,.navbar-fixed-top');
+    //    var isShowhing  = !(rightBar.data('displayed') == 1);
+    //
+    //    rightBar.css('right', !isShowhing ? 0 : -300);
+    //    toMove.css({
+    //        'margin-right': !isShowhing ? 300 : 0,
+    //        'margin-left': !isShowhing ? -300 : 0
+    //    });
+    //    rightBar.animate({
+    //        right: !isShowhing ? -300 : 0
+    //    }, {duration: 200, queue: false});
+    //    toMove.animate({
+    //        marginRight: isShowhing ? 300 : 0,
+    //        marginLeft: isShowhing ? -300 : 0
+    //    }, {duration: 200, queue: false});
+    //    rightBar.data('displayed', isShowhing);
+    //});
 })();
